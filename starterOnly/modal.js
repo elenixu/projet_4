ed081e170bbacf01f5d8a30fbd1aeeef4b26f4d5
GIL SALAZAR Elena <you@example.com>
2024-10-24 13:52:00 +0200

    [Add] Validation function nom prenom email

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -30,3 +30,36 @@ function closeModal() {
 }
 
 
+// Validate function
+
+function validate() {
+  // Fetch values of first and last name fields
+  var firstName = document.getElementById("first").value;
+  var lastName = document.getElementById("last").value;
+  var email = document.getElementById ("email").value;
+
+  // Check if the first name is at least 2 characters long
+  if (firstName.length < 2) {
+    alert("Le champ Prénom doit contenir au moins 2 caractères.");
+    return false; 
+  }
+
+  // Check if the last name is at least 2 characters long
+  if (lastName.length < 2) {
+    alert("Le champ Nom doit contenir au moins 2 caractères.");
+    return false; 
+  }
+
+  // Email validation with regex pattern
+  var emailAdress = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  if (!emailAdress.test(email)) {
+    alert("Veuillez saisir une adresse e-mail valide.");
+    return false;
+  }
+
+
+  return true;
+}
+
+
+
